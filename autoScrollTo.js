134718cb886dfda222c6ce46098e1bb70395f2a6
HarryGoodwin <harrygoodwin0@gmail.com>
2016-08-17 21:47:21 +1000

    Fixed autoscroll animation bug

--- autoScrollTo.js
+++ autoScrollTo.js
@@ -1,28 +1,30 @@
 var scrollY = 0;
-var distance = 40;
+var distance = 10;
 var speed = 24;
+div1TopOffset = 10;
 
 function autoScrollTo(el) {
 	var currentY = window.pageYOffset;
-	var targetY = document.getElementById(el).offsetTop;
+	var targetY = document.getElementById(el).getBoundingClientRect().top;
 	var bodyHeight = document.body.offsetHeight;
 	var yPos = currentY + window.innerHeight;
-	var animator = setTimeout('autoScrollTo(\''+el+'\')', 24);
+	var animator = setTimeout('autoScrollTo(\''+el+'\')', 5);
 
 	if(yPos > bodyHeight){
 		clearTimeout(animator);
 		console.log(yPos);
+        
+        return
 	}
-	else {
-		if (currentY < targetY-distance) {
-			scrollY = currentY+distance;
-			window.scroll(0, scrollY);
-			console.log(yPos);
-		}
-		else {
-			clearTimeout(animator);
-			console.log(yPos);
-		}
-	}
+    
+    if (targetY > div1TopOffset) {
+        scrollY = currentY+distance;
+        window.scroll(0, scrollY);
+        console.log(yPos);
+    }
+    else {
+        clearTimeout(animator);
+        console.log(yPos);
+    }
 }		
 
