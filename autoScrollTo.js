d68340e9eafdb6047309b9100447e057c573acea
HarryGoodwin <harrygoodwin0@gmail.com>
2016-08-18 21:39:04 +1000

    Added or equal to operator in if condtion for when ypos reaches end of page

--- autoScrollTo.js
+++ autoScrollTo.js
@@ -9,22 +9,30 @@ function autoScrollTo(el) {
 	var bodyHeight = document.body.offsetHeight;
 	var yPos = currentY + window.innerHeight;
 	var animator = setTimeout('autoScrollTo(\''+el+'\')', 5);
+    
+    console.log("CurrentY:"+currentY+", targetY:"+targetY+", BodyHeight:"+bodyHeight+", Ypos:"+yPos)
 
-	if(yPos > bodyHeight){
+	if(yPos >= bodyHeight){
 		clearTimeout(animator);
 		console.log(yPos);
         
+        console.log("ypos bigger than bodyHeight - returning")
         return
 	}
     
     if (targetY > div1TopOffset) {
+        console.log("targety bigger than div§ToppOffset")
+        console.log("TargetY:"+targetY)
         scrollY = currentY+distance;
+        console.log("scrollY:"+scrollY)
         window.scroll(0, scrollY);
-        console.log(yPos);
+        console.log("yPos:"+yPos);
     }
     else {
+        "targetY less than div1Offset - clearingTimeout"
         clearTimeout(animator);
         console.log(yPos);
     }
 }		
+
 
